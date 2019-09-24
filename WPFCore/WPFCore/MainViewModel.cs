b49e3b2ea65afc97214357928b6de909079347c9
Andre <ruland@outlook.de>
2019-09-24 16:51:29 +0200

    WPF Core

---
+++ WPFCore/WPFCore/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFCore
+{
+    class MainViewModel
+    {
+        public List<Pillen> PillenListe { get; set; } = new List<Pillen>();
+
+        public Pillen SelectedPille { get; set; }
+
+
+
+        public MainViewModel()
+        {
+            PillenListe.Add(new Pillen() { Id = 1, Name = "Aspirin", Gewicht = 623 });
+            PillenListe.Add(new Pillen() { Id = 2, Name = "Ibu", Gewicht = 1233 });
+            PillenListe.Add(new Pillen() { Id = 3, Name = "Prägni", Gewicht = -265 });
+        }
+    }
+}
---
+++ WPFCore/WPFCore/Pillen.cs
@@ -0,0 +1,9 @@
+﻿namespace WPFCore
+{
+    class Pillen
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Gewicht { get; set; }
+    }
+}
