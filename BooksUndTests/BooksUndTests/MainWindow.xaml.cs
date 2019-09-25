aeb84a1169d10770ce1b88b842bd63f8fc24acc7
Andre <ruland@outlook.de>
2019-09-25 14:16:44 +0200

    Bücker WPF JSOn Google .net Core 3.0

---
+++ BooksUndTests/BooksUndTests/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using System.Windows;
+
+namespace BooksUndTests
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LadenBücher(object sender, RoutedEventArgs e)
+        {
+            using (var wc = new WebClient())
+            {
+                var json = wc.DownloadString($"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}");
+
+                //newtonsoft
+                var result = JsonConvert.DeserializeObject<BooksResult>(json);
+
+                //core 3.0
+                var result2 = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+                myGrid.ItemsSource = result2.items.Select(x => x.volumeInfo);
+
+            }
+        }
+    }
+}
